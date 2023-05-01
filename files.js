64e590c59dcf104cb6282d7debfe3ac44c7fb637
student <adi0548426024@gmail.com>
2023-05-01 13:17:35 +0300

    init

---
+++ files.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+const stream = fd.createReadStream
+
+fs.writeFile('./files/adi.txt',"adi buchris",()=>{
+    console.log("written");
+})
---
+++ server.js
@@ -0,0 +1,16 @@
+
+ 
+
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('Hello')
+})
+
+app.get('/test',(req,res)=>{
+    res.send('Test hello')
+})
+
+module.exports = app;
