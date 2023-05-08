3a96f8bbf141897896244ea6169fd710dd3a61a8
student <adi0548426024@gmail.com>
2023-05-08 16:29:21 +0300

    .

--- src/server.js
+++ src/server.js
@@ -24,13 +24,13 @@ app.post('/registration', (req, res) => {
     console.log('Grade 1:', grade1);
     console.log('Grade 2:', grade2);
     console.log('Grade 3:', grade3);
-     // Write the data to a file
+    // Write the data to a file
     const data = `Name: ${name}\nGrade 1: ${grade1}\nGrade 2: ${grade2}\nGrade 3: ${grade3}\n\n`;
     fs.appendFile('registration.txt', data, (err) => {
         if (err) {
-        console.error('Error writing to file:', err);
+            console.error('Error writing to file:', err);
         } else {
-        console.log('Data written to file');
+            console.log('Data written to file');
         }
     });
     // Return a response indicating the registration was successful
