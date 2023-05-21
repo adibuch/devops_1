af883ea67cb983ff4997e70b7000b643a6e2fb3d
student <adi0548426024@gmail.com>
2023-05-21 16:12:36 +0300

    .

--- test/try.test.js
+++ test/try.test.js
@@ -1,53 +1,46 @@
-
 const request = require('supertest');
-const app = require('../src/server');
+const app = require('../src/server'); // Replace 'your-app-file' with the actual file name
 
-
-describe("Test suite 1:",()=>{
-    test("test 1: ",async()=>{
-        const res = await request(app).get('/')
-        expect(res.statusCode).toEqual(200)
-    })
-    test("test 2: ",async()=>{
-        const res = await request(app).get('/1234')
-        expect(res.statusCode).toEqual(404)
-    })
-})
-
-
-describe('Registration endpoint', () => {
-  test('should handle the registration POST request', async () => {
-    const response = await request(app)
-      .post('/registration')
-      .send({
-        name: 'John Doe',
-        grade1: 90,
-        grade2: 85,
-        grade3: 95
-      });
-    
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual({ message: 'Registration successful' });
-      // Add a delay to allow the file operation to complete
-    await new Promise((resolve) => setTimeout(resolve, 100));
+describe('Test your Express app', () => {
+  // Test the root endpoint
+  it('should return the index page', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    // Add more assertions if necessary
   });
 
+  // Test the registration endpoint
+  it('should register a student successfully', async () => {
+    const studentData = {
+      name: 'John Doe',
+      grade1: 90,
+      grade2: 85,
+      grade3: 95
+    };
 
-test('POST /registration with incomplete form data should return an error', async () => {
     const response = await request(app)
       .post('/registration')
-      .send({
-        name: 'John Doe',
-        grade1: 90,
-        // Missing grade2 and grade3
-      });
-  
-    expect(response.statusCode).toBe(400);
-    expect(response.body).toEqual({ error: 'Incomplete form data' });
-    
+      .send(studentData);
+
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('Registration successful');
+    // Add more assertions if necessary
+  }, 10000);
+
+  // Test the registration endpoint with incomplete form data
+  it('should return an error for incomplete form data', async () => {
+    const incompleteData = {
+      name: 'John Doe',
+      grade1: 90
+      // Missing grade2 and grade3 intentionally
+    };
+
+    const response = await request(app)
+      .post('/registration')
+      .send(incompleteData);
+
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBe('Incomplete form data');
+    // Add more assertions if necessary
   });
 });
-
-
-
-  
